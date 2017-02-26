f02810457b211f3c830c6037665a82e788e98157
Tom <Tom@TOMS-DESKTOP>
2017-02-26 14:24:15 -0500

    Added set of examples on how to use the HTTPHelper class in C#.

---
+++ C-Sharp Example/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace C_Sharp_Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.WebBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnDownloadFile2 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.btnStopDownload = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnDownloadFile = new System.Windows.Forms.Button();
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.postDataExample = new System.Windows.Forms.Button();
+            this.btnGetWebPageData = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBox1.Location = new System.Drawing.Point(12, 383);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox1.Size = new System.Drawing.Size(804, 186);
+            this.TextBox1.TabIndex = 4;
+            // 
+            // WebBrowser1
+            // 
+            this.WebBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WebBrowser1.Location = new System.Drawing.Point(12, 108);
+            this.WebBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser1.Name = "WebBrowser1";
+            this.WebBrowser1.Size = new System.Drawing.Size(804, 269);
+            this.WebBrowser1.TabIndex = 5;
+            // 
+            // btnDownloadFile2
+            // 
+            this.btnDownloadFile2.Location = new System.Drawing.Point(558, 41);
+            this.btnDownloadFile2.Name = "btnDownloadFile2";
+            this.btnDownloadFile2.Size = new System.Drawing.Size(222, 23);
+            this.btnDownloadFile2.TabIndex = 21;
+            this.btnDownloadFile2.Text = "Multi-Threaded Download File";
+            this.btnDownloadFile2.UseVisualStyleBackColor = true;
+            this.btnDownloadFile2.Click += new System.EventHandler(this.btnDownloadFile2_Click);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(458, 67);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(39, 13);
+            this.Label2.TabIndex = 20;
+            this.Label2.Text = "Label2";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(558, 79);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(222, 23);
+            this.Button1.TabIndex = 19;
+            this.Button1.Text = "Button1";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // btnStopDownload
+            // 
+            this.btnStopDownload.Enabled = false;
+            this.btnStopDownload.Location = new System.Drawing.Point(672, 12);
+            this.btnStopDownload.Name = "btnStopDownload";
+            this.btnStopDownload.Size = new System.Drawing.Size(108, 23);
+            this.btnStopDownload.TabIndex = 18;
+            this.btnStopDownload.Text = "Stop Download";
+            this.btnStopDownload.UseVisualStyleBackColor = true;
+            this.btnStopDownload.Click += new System.EventHandler(this.btnStopDownload_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(340, 12);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 17;
+            this.btnUpload.Text = "Upload File";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 67);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(39, 13);
+            this.Label1.TabIndex = 16;
+            this.Label1.Text = "Label1";
+            // 
+            // btnDownloadFile
+            // 
+            this.btnDownloadFile.Location = new System.Drawing.Point(558, 12);
+            this.btnDownloadFile.Name = "btnDownloadFile";
+            this.btnDownloadFile.Size = new System.Drawing.Size(108, 23);
+            this.btnDownloadFile.TabIndex = 15;
+            this.btnDownloadFile.Text = "Download File";
+            this.btnDownloadFile.UseVisualStyleBackColor = true;
+            this.btnDownloadFile.Click += new System.EventHandler(this.btnDownloadFile_Click);
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(12, 41);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(485, 23);
+            this.ProgressBar1.TabIndex = 14;
+            // 
+            // postDataExample
+            // 
+            this.postDataExample.Location = new System.Drawing.Point(176, 12);
+            this.postDataExample.Name = "postDataExample";
+            this.postDataExample.Size = new System.Drawing.Size(158, 23);
+            this.postDataExample.TabIndex = 13;
+            this.postDataExample.Text = "Post Data to Web Site";
+            this.postDataExample.UseVisualStyleBackColor = true;
+            this.postDataExample.Click += new System.EventHandler(this.postDataExample_Click);
+            // 
+            // btnGetWebPageData
+            // 
+            this.btnGetWebPageData.Location = new System.Drawing.Point(12, 12);
+            this.btnGetWebPageData.Name = "btnGetWebPageData";
+            this.btnGetWebPageData.Size = new System.Drawing.Size(158, 23);
+            this.btnGetWebPageData.TabIndex = 12;
+            this.btnGetWebPageData.Text = "Get Web Page Data";
+            this.btnGetWebPageData.UseVisualStyleBackColor = true;
+            this.btnGetWebPageData.Click += new System.EventHandler(this.btnGetWebPageData_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 581);
+            this.Controls.Add(this.btnDownloadFile2);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.btnStopDownload);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.btnDownloadFile);
+            this.Controls.Add(this.ProgressBar1);
+            this.Controls.Add(this.postDataExample);
+            this.Controls.Add(this.btnGetWebPageData);
+            this.Controls.Add(this.WebBrowser1);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.TextBox TextBox1;
+        internal System.Windows.Forms.WebBrowser WebBrowser1;
+        internal System.Windows.Forms.Button btnDownloadFile2;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Button btnStopDownload;
+        internal System.Windows.Forms.Button btnUpload;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button btnDownloadFile;
+        internal System.Windows.Forms.ProgressBar ProgressBar1;
+        internal System.Windows.Forms.Button postDataExample;
+        internal System.Windows.Forms.Button btnGetWebPageData;
+        internal System.Windows.Forms.OpenFileDialog OpenFileDialog;
+    }
+}
+
---
+++ C-Sharp Example/Form1.cs
@@ -0,0 +1,312 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+
+namespace C_Sharp_Example
+{
+    public partial class Form1 : Form
+    {
+        delegate void myCustomErrorHandlerDelegate(Exception ex, httpHelper classInstance);
+        delegate void myDownloadStatusRoutine(downloadStatusDetails downloadStatusDetails);
+        System.Threading.Thread downloadThread;
+        System.Threading.Thread statusThread;
+        ulong oldFileSize = 0;
+        string urlToDownload = "http://releases.ubuntu.com/16.04.2/ubuntu-16.04.2-desktop-amd64.iso";
+        string localFilePathToDownloadFileTo = "S:\\ubuntu-16.04.2-desktop-amd64.iso";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetWebPageData_Click(object sender, EventArgs e)
+        {
+            try {
+                string strServerResponse = null;
+
+                httpHelper httpHelper = new httpHelper();
+                httpHelper.setUserAgent = "Microsoft .NET"; // Set our User Agent String.
+                httpHelper.addGETData("test3", "value3");
+                httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/");
+                httpHelper.addHTTPHeader("myheader", "my header contents");
+                httpHelper.setHTTPCredentials("test", "test");
+
+                // This sets up our custom error handler delegate to be injected like a plugin into the HTTPHelper class instance.
+                    myCustomErrorHandlerDelegate setCustomErrorHandler = (Exception ex, httpHelper classInstance) => { Interaction.MsgBox(ex.Message); };
+                    httpHelper.setCustomErrorHandler = setCustomErrorHandler;
+                // This sets up our custom error handler delegate to be injected like a plugin into the HTTPHelper class instance.
+
+                if (httpHelper.getWebData("https://www.toms-world.org/php/phpinfo.php", ref strServerResponse)) {
+                    WebBrowser1.DocumentText = strServerResponse;
+                    TextBox1.Text = httpHelper.getHTTPResponseHeaders(true).ToString();
+
+                    X509Certificate2 certDetails = httpHelper.getCertificateDetails(false);
+                    if (certDetails != null) {
+                        TextBox1.Text += certDetails.ToString();
+                    }
+                }
+            }
+            catch (httpProtocolException ex) {
+                // You can handle httpProtocolExceptions different than normal exceptions with this code.
+            }
+            catch (System.Net.WebException ex) {
+                // You can handle web exceptions different than normal exceptions with this code.
+            }
+            catch (Exception ex) {
+                Interaction.MsgBox(ex.Message + " " + ex.StackTrace);
+            }
+        }
+
+        private void postDataExample_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strServerResponse = null;
+
+                httpHelper httpHelper = new httpHelper();
+                httpHelper.setUserAgent = "Microsoft .NET"; // Set our User Agent String.
+                httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/");
+                httpHelper.addHTTPHeader("myheader", "my header contents");
+                httpHelper.addPOSTData("test1", "value1");
+                httpHelper.addPOSTData("test2", "value2");
+                httpHelper.addGETData("test3", "value3");
+                httpHelper.addPOSTData("major", "3");
+                httpHelper.addPOSTData("minor", "9");
+                httpHelper.addPOSTData("build", "6");
+
+                if (httpHelper.getWebData("https://www.toms-world.org/httphelper.php", ref strServerResponse))
+                {
+                    WebBrowser1.DocumentText = strServerResponse;
+                    TextBox1.Text = httpHelper.getHTTPResponseHeaders().ToString();
+
+                    X509Certificate2 certDetails = httpHelper.getCertificateDetails(false);
+                    if (certDetails != null)
+                    {
+                        TextBox1.Text += certDetails.ToString();
+                    }
+
+                    //For Each strHeaderName As String In httpHelper.getHTTPResponseHeaders
+                    //    MsgBox(strHeaderName & " = " & httpHelper.getHTTPResponseHeaders.Item(strHeaderName))
+                    //Next
+                }
+            }
+            catch (httpProtocolException ex)
+            {
+                // You can handle httpProtocolExceptions different than normal exceptions with this code.
+            }
+            catch (System.Net.WebException ex)
+            {
+                // You can handle web exceptions different than normal exceptions with this code.
+            }
+            catch (Exception ex)
+            {
+                Interaction.MsgBox(ex.Message + " " + ex.StackTrace);
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog.Title = "Browse for file to upload...";
+                OpenFileDialog.FileName = null;
+                OpenFileDialog.Filter = "Image Files (JPEG, PNG)|*.png;*.jpg;*.jpeg";
+
+                if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string strServerResponse = null;
+
+                    httpHelper httpHelper = new httpHelper();
+                    httpHelper.setHTTPTimeout = 10;
+                    httpHelper.setUserAgent = "Microsoft .NET"; // Set our User Agent String.
+                    httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/");
+                    httpHelper.addHTTPHeader("myheader", "my header contents");
+                    httpHelper.addPOSTData("test1", "value1");
+                    httpHelper.addPOSTData("test2", "value2");
+                    httpHelper.addGETData("test3", "value3");
+                    httpHelper.addFileUpload("myfileupload", OpenFileDialog.FileName, null, null);
+
+                    if (httpHelper.uploadData("https://www.toms-world.org/httphelper.php", ref strServerResponse))
+                    {
+                        WebBrowser1.DocumentText = strServerResponse;
+                        TextBox1.Text = httpHelper.getHTTPResponseHeaders().ToString();
+
+                        X509Certificate2 certDetails = httpHelper.getCertificateDetails(false);
+                        if (certDetails != null)
+                        {
+                            TextBox1.Text += certDetails.ToString();
+                        }
+                    }
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                Interaction.MsgBox(ex.Message + " " + ex.StackTrace);
+            }
+        }
+
+        private void btnStopDownload_Click(object sender, EventArgs e)
+        {
+            downloadThread.Abort();
+            if (statusThread != null) { statusThread.Abort(); }
+        }
+
+        private void btnDownloadFile_Click(object sender, EventArgs e)
+        {
+            // First we create our httpHelper Class instance.
+            {
+                httpHelper httpHelper = new httpHelper();
+                httpHelper.setUserAgent = "Microsoft .NET"; // Set our User Agent String.
+                httpHelper.enableMultiThreadedDownloadStatusUpdates = true;
+                ulong oldFileSize = 0;
+                
+                myDownloadStatusRoutine myDownloadStatusUpdater = (downloadStatusDetails downloadStatusDetails) => {
+                    if (httpHelper.enableMultiThreadedDownloadStatusUpdates) {
+                        Label1.Text = string.Format("Downloaded {0} of {1} ({2}/s)", httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.remoteFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize - (long)oldFileSize));
+                        oldFileSize = (ulong)downloadStatusDetails.localFileSize;
+                    }
+                    else {
+                        Label1.Text = string.Format("Downloaded {0} of {1}", httpHelper.fileSizeToHumanReadableFormat((long)downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat((long)downloadStatusDetails.remoteFileSize));
+                    }
+
+                    Label2.Text = downloadStatusDetails.percentageDownloaded.ToString() + "%";
+                    ProgressBar1.Value = downloadStatusDetails.percentageDownloaded;
+                };
+                httpHelper.setDownloadStatusUpdateRoutine = myDownloadStatusUpdater;
+
+                // Now we need to create our download thread.
+                downloadThread = new System.Threading.Thread(() => {
+                    string urlToFileToBeDownloaded = urlToDownload;
+                    string pathToDownloadFileTo = localFilePathToDownloadFileTo;
+                    System.IO.MemoryStream memStream = new System.IO.MemoryStream();
+
+                    try {
+                        btnStopDownload.Enabled = true;
+                        btnDownloadFile.Enabled = false;
+                        btnDownloadFile2.Enabled = false;
+
+                        // We use the downloadFile() function which first calls for the URL and then the path to a place on the local file system to save it. This function is why we need multithreading, this will take a long time to do.
+                        if (httpHelper.downloadFile(urlToFileToBeDownloaded, ref memStream, true)) {
+                            System.IO.FileStream fileStream = new System.IO.FileStream(pathToDownloadFileTo, System.IO.FileMode.Create);
+                            memStream.CopyTo(fileStream);
+                            memStream.Close();
+                            memStream.Dispose();
+                            fileStream.Close();
+                            fileStream.Dispose();
+
+                            btnDownloadFile.Enabled = true;
+                            btnStopDownload.Enabled = false;
+                            Interaction.MsgBox("Download complete.");
+                            // And tell the user that the download is complete.
+                        }
+
+                    }
+                    catch (System.Net.WebException ex) {
+                        btnDownloadFile.Enabled = true;
+                        btnDownloadFile2.Enabled = true;
+                        btnStopDownload.Enabled = false;
+                        Interaction.MsgBox(ex.Message + " " + ex.StackTrace);
+
+                    }
+                    catch (System.Threading.ThreadAbortException ex) {
+                        btnDownloadFile.Enabled = true;
+                        btnDownloadFile2.Enabled = true;
+                        btnStopDownload.Enabled = false;
+
+                        if (System.IO.File.Exists(pathToDownloadFileTo)) System.IO.File.Delete(pathToDownloadFileTo);
+                        Interaction.MsgBox("Download aborted.");
+                        // And tell the user that the download is aborted.
+                    }
+                });
+
+                // Starts our download thread.
+                downloadThread.IsBackground = true;
+                downloadThread.Start();
+            }
+        }
+
+        private void btnDownloadFile2_Click(object sender, EventArgs e)
+        {
+            // First we create our httpHelper Class instance.
+            {
+                httpHelper httpHelper = new httpHelper();
+                httpHelper.setUserAgent = "Microsoft .NET"; // Set our User Agent String.
+
+                // Now we need to create our download thread.
+                downloadThread = new System.Threading.Thread(() => {
+                    string urlToFileToBeDownloaded = urlToDownload;
+                    string pathToDownloadFileTo = localFilePathToDownloadFileTo;
+                    System.IO.MemoryStream memStream = new System.IO.MemoryStream();
+
+                    try {
+                        btnStopDownload.Enabled = true;
+                        btnDownloadFile.Enabled = false;
+                        btnDownloadFile2.Enabled = false;
+
+                        // We use the downloadFile() function which first calls for the URL and then the path to a place on the local file system to save it. This function is why we need multithreading, this will take a long time to do.
+                        if (httpHelper.downloadFile(urlToFileToBeDownloaded, ref memStream, true)) {
+                            System.IO.FileStream fileStream = new System.IO.FileStream(pathToDownloadFileTo, System.IO.FileMode.Create);
+                            memStream.CopyTo(fileStream);
+                            memStream.Close();
+                            memStream.Dispose();
+                            fileStream.Close();
+                            fileStream.Dispose();
+
+                            btnDownloadFile.Enabled = true;
+                            btnStopDownload.Enabled = false;
+                            Interaction.MsgBox("Download complete.");
+                            // And tell the user that the download is complete.
+                        }
+
+                    }
+                    catch (System.Net.WebException ex) {
+                        btnDownloadFile.Enabled = true;
+                        btnDownloadFile2.Enabled = true;
+                        btnStopDownload.Enabled = false;
+                        Interaction.MsgBox(ex.Message + " " + ex.StackTrace);
+
+                    }
+                    catch (System.Threading.ThreadAbortException ex) {
+                        btnDownloadFile.Enabled = true;
+                        btnDownloadFile2.Enabled = true;
+                        btnStopDownload.Enabled = false;
+                        if (System.IO.File.Exists(pathToDownloadFileTo)) System.IO.File.Delete(pathToDownloadFileTo);
+                        Interaction.MsgBox("Download aborted.");
+                        // And tell the user that the download is aborted.
+                    }
+                });
+
+                downloadThread.IsBackground = true;
+                downloadThread.Start();
+                // Starts our download thread.
+
+                statusThread = new System.Threading.Thread(() => {
+                    downloadStatusDetails downloadStatusDetails;
+                    startAgain:
+                    downloadStatusDetails = httpHelper.getDownloadStatusDetails;
+
+                    if (downloadStatusDetails != null) {
+                        Label1.Text = string.Format("Downloaded {0} of {1} ({2}/s)", httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.remoteFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize - (long)oldFileSize));
+
+                        oldFileSize = (ulong)downloadStatusDetails.localFileSize;
+
+                        Label2.Text = downloadStatusDetails.percentageDownloaded.ToString() + "%";
+                        ProgressBar1.Value = downloadStatusDetails.percentageDownloaded;
+                    }
+                
+                    System.Threading.Thread.Sleep(1000);
+                    goto startAgain;
+                });
+
+                statusThread.IsBackground = true;
+                statusThread.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
