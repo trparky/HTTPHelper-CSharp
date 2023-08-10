2233a51a5968bc44eaee8f505cf1fad76a1e18c7
Tom <Tom@TOMS-DESKTOP>
2023-08-10 14:56:07 -0400

    Updated copyright in AssemblyInfo.cs.

--- HTTPHelper C-Sharp/Properties/AssemblyInfo.cs
+++ HTTPHelper C-Sharp/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HTTPHelper C-Sharp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Thomas R. Parkison 2023-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
